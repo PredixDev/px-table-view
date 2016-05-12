561f5cfcd72e34c2594208736b11988f7c1d126f
Jonnie Spratley <jonnie.spratley@ge.com>
2016-05-11 19:49:32 -0700

    fix: updated css

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,7 @@ const pkg = require('./package.json');
 const $ = require('gulp-load-plugins')();
 const gulpSequence = require('gulp-sequence');
 const sassdoc = require('sassdoc');
+const filelog = require('gulp-filelog');
 const importOnce = require('node-sass-import-once');
 require('web-component-tester').gulp.init(gulp);
 const sassdocOptions = {
@@ -27,46 +28,56 @@ const sassOptions = {
   }
 };
 
-gulp.task('sassdoc', function () {
+gulp.task('sassdoc', function() {
   return gulp.src('sass/**/*.scss')
-    .pipe(sassdoc(sassdocOptions));
+    //  .pipe($.filelog())
+    .pipe(sassdoc(sassdocOptions))
+    .pipe($.filelog());
 });
 
-gulp.task('clean', function () {
+gulp.task('clean', function() {
   return gulp.src(['.tmp', 'css'], {
-    read: false
-  }).pipe($.clean());
+      read: false
+    })
+    .pipe($.filelog())
+    .pipe($.clean());
 });
 
-gulp.task('sass', function () {
+gulp.task('sass', function() {
   return gulp.src('./sass/**/*.scss')
+    //  .pipe($.filelog())
     .pipe($.sass(sassOptions).on('error', $.sass.logError))
     .pipe($.size())
     .pipe($.concat(pkg.name + '.css'))
-    .pipe(gulp.dest('./css'));
+    .pipe(gulp.dest('./css'))
+    .pipe($.filelog());
 });
 
-gulp.task('autoprefixer', function () {
+gulp.task('autoprefixer', function() {
   return gulp.src('css/*.css')
+    //  .pipe($.filelog())
     .pipe($.autoprefixer({
       browsers: ['last 2 versions'],
       cascade: false
     }))
     .pipe($.size())
-    .pipe(gulp.dest('css'));
+    .pipe(gulp.dest('css'))
+    .pipe($.filelog());
 });
 
-gulp.task('css', function () {
+gulp.task('css', function() {
   return gulp.src('css/*.css')
     .pipe($.sourcemaps.init())
     .pipe($.cssmin())
+    .pipe($.filelog())
     .pipe($.concat(pkg.name + '.css'))
     .pipe($.sourcemaps.write('.'))
     .pipe($.rename({
       suffix: '.min'
     }))
     .pipe($.size())
-    .pipe(gulp.dest('css'));
+    .pipe(gulp.dest('css'))
+    .pipe($.filelog());
 });
 
 
@@ -74,7 +85,7 @@ gulp.task('css', function () {
 gulp.task('build:styles', gulpSequence('sass', 'autoprefixer'));
 
 
-gulp.task('sass:watch', function () {
+gulp.task('sass:watch', function() {
   gulp.watch('./sass/**/*.scss', ['sass']);
   gulp.watch('./css/**/*.css', ['autoprefixer']);
 });
