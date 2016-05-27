bfb9f034c22d690faa97056c5ee932e2024405b9
Jonnie Spratley <jonnie.spratley@ge.com>
2016-05-26 19:56:22 -0700

    fix: updated gp

--- test/px-table-view-custom-tests.js
+++ test/px-table-view-custom-tests.js
@@ -4,30 +4,14 @@ function runCustomTests() {
 
   // This is the placeholder suite to place custom tests in
   // Use testCase(options) for a more convenient setup of the test cases
-  suite('<px-table-view>', function () {
+  suite('<px-table-view>', function() {
     var element = document.getElementById('px_table_view_1');
     element.items = [{
       title: 'Item 1'
-		}];
-    test('renders', function () {
+    }];
+    test('renders', function() {
       assert.ok(element);
     });
-    test('has correct # of items', function () {
-      assert.ok(element.items[0].title = 'Item 1');
-    });
-    test('can add item to element', function () {
-      element.push('items', {
-        title: 'Item 2',
-        href: '#'
-      });
-      element.push('items', {
-        title: 'Item 3',
-        href: '#'
-      });
-      assert.ok(element.items.length === 3);
-    });
-    test('has correct # of items', function () {
-      assert.ok(element.items.length === 3);
-    });
+
   });
 }
